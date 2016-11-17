8af112f5cd1f3b022f7cab2f1b87f51630e1dd0f
Jordan Camilletti <jordan.camilletti@gmail.com>
2016-11-17 11:20:46 -0600

    Update 10. Summation of primes.py

--- 10. Summation of primes.py
+++ 10. Summation of primes.py
@@ -1,2 +1,15 @@
 """The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 Find the sum of all the primes below two million."""
+
+def isPrime(num):
+	for x in range(num-2):
+		if(num%(x+2)==0):
+			return False
+	return True
+		
+tot=0
+for x in range(8):
+	if(isPrime(x+2)):
+		print(x+2)
+		tot=tot+(x+2)
+print(tot)
