eda57e7df8f34b692696c5f993e4c90cc5495811
Jordan Camilletti <jordan.camilletti@gmail.com>
2016-11-17 09:32:44 -0600

    Update 9. Special Pythagorean triplet.py

--- 9. Special Pythagorean triplet.py
+++ 9. Special Pythagorean triplet.py
@@ -3,3 +3,12 @@ a2 + b2 = c2
 For example, 32 + 42 = 9 + 16 = 25 = 52.
 There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 Find the product abc."""
+
+def pythag(a,b,c):
+  if((a*a)+(b*b)==c*c):
+    return True
+  return False
+
+a=3,b=4,c=5
+if(pythag(a,b,c)):
+  print(a+b+c)
