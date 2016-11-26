322822601dc6b8c9c963a45936c8d0260ef0ac0e
Jordan Camilletti <jordan.camilletti@gmail.com>
2016-11-26 14:53:47 -0600

    Update 7. 10001st prime.py

--- 7. 10001st prime.py
+++ 7. 10001st prime.py
@@ -1,2 +1,8 @@
 """By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
 What is the 10 001st prime number?"""
+
+def isPrime(n):
+    for x in range(n-2):
+        if(n%(x+2)==0):
+            return False
+    return True
