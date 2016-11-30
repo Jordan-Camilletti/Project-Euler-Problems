9a1d6009b4be3927a749d4dea13dfa2b94584fcf
Jordan Camilletti <jordan.camilletti@gmail.com>
2016-11-30 09:07:48 -0600

    Update 9. Special Pythagorean triplet.py

--- 9. Special Pythagorean triplet.py
+++ 9. Special Pythagorean triplet.py
@@ -8,5 +8,16 @@ def pythag(a,b,c):
   if((a*a)+(b*b)==c*c):
     return True
   return False
-
-//WORK ON THIS LATER
+  
+a=4
+b=3
+c=2
+while(a<=335):
+	a+=1
+	while(b<a):
+		b+=1
+		while(c<b):
+			c+=1
+			print("c:",c)
+			print("b:",b)
+			print("a:",a)
