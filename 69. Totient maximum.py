8ac4ae41a3be8876fe3dadcc111988ce8729b367
Jordan Camilletti <jordan.camilletti@gmail.com>
2016-12-02 08:43:22 -0600

    Update 69. Totient maximum.py

--- 69. Totient maximum.py
+++ 69. Totient maximum.py
@@ -1,2 +1,36 @@
 """Euler's Totient function, φ(n), is used to determine the number of numbers less than n which are relatively prime to n. 
-For example, as 1, 2, 4, 5, 7, and 8, are all less than nine and relatively prime to nine, φ(9)=6."""
+For example, as 1, 2, 4, 5, 7, and 8, are all less than nine and relatively prime to nine, φ(9)=6.
+It can be seen that n=6 produces a maximum n/φ(n) for n ≤ 10.
+Find the value of n ≤ 1,000,000 for which n/φ(n) is a maximum"""
+
+def reletivPrime(a,b):
+	top=1
+	if(a>b):
+	    for i in range(b-1):
+	        if(a%(i+2)==0 and b%(i+2)==0):
+	            return False
+		return True
+	else:
+		for i in range(a-1):
+		    if(a%(i+2)==0 and b%(i+2)==0):
+		        return False
+		return True
+
+def phi(n):
+    tot=0
+    for x in range(n-1):
+        if(reletivPrime(x+1,n)):
+			tot=tot+1
+    return(tot)
+    
+top=0
+n=0
+for nem in range(9):
+    num=nem+2
+    p=phi(num)
+    if(num/p>top):
+        top=num/p
+        n=num
+
+print("n",n)
+print("top",top)
