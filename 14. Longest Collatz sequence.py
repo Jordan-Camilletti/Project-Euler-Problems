c2df5363e3f2ed659dce24b31ee092171dd3e268
Jordan Camilletti <jordan.camilletti@gmail.com>
2016-12-23 15:17:04 -0600

    Update 14. Longest Collatz sequence.py

--- 14. Longest Collatz sequence.py
+++ 14. Longest Collatz sequence.py
@@ -8,10 +8,8 @@ Using the rule above and starting with 13, we generate the following sequence:
 13 → 40 → 20 → 10 → 5 → 16 → 8 → 4 → 2 → 1
 It can be seen that this sequence (starting at 13 and finishing at 1) contains 10 terms. Although it has not been proved yet (Collatz Problem), it is thought that all starting numbers finish at 1.
 
-Which starting number, under one million, produces the longest chain?"""
-
-topn=0
-top=0
+Which starting number, under one million, produces the longest chain?"""topNum=0
+topChain=0
 for n1 in range(999999):
 	n=n1+1
 	tot=1
@@ -21,7 +19,7 @@ for n1 in range(999999):
 			n=n/2
 		else:
 			n=(3*n)+1
-	if(tot>top):
-		top=tot
-		topn=n1+1
-print(topn)
+	if(tot>topChain):
+		topChain=tot
+		topNum=n1+1
+print(topNum)
