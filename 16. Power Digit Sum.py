876bfd4f07a74357607e6f77b96a7da48f16712b
Jordan Camilletti <jordan.camilletti@gmail.com>
2016-12-29 13:19:28 -0600

    Update 16. Power Digit Sum.py

--- 16. Power Digit Sum.py
+++ 16. Power Digit Sum.py
@@ -1,7 +1,7 @@
 """2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
 What is the sum of the digits of the number 2^1000?"""
 
-str=str(2**15)
+str=str(2**1000)
 tot=0
 for x in str:
 	tot=tot+int(x)
