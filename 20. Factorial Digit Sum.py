d915fe7d38a3ef25ee92dd48bbae30e9d43628a5
Jordan Camilletti <jordan.camilletti@gmail.com>
2016-12-30 12:01:21 -0600

    Update 20. Factorial Digit Sum.py

--- 20. Factorial Digit Sum.py
+++ 20. Factorial Digit Sum.py
@@ -1,6 +1,13 @@
-/*n! means n × (n − 1) × ... × 3 × 2 × 1
+"""n! means n × (n − 1) × ... × 3 × 2 × 1
 
 For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
 and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
 
-Find the sum of the digits in the number 100!*/
+Find the sum of the digits in the number 100!"""
+tot=1
+sum=0
+for x in range(100):
+	tot=tot*(x+1)
+for x in str(tot):
+	sum=sum+int(x)
+print(sum)
