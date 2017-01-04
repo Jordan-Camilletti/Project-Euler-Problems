41b42b4178bd0e9cac96ba3bcd2e53a6489d4926
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-01-04 11:50:26 -0600

    Update 35. Circular Primes.py

--- 35. Circular Primes.py
+++ 35. Circular Primes.py
@@ -1,6 +1,12 @@
 """
 """
 
+def isPrime(num):
+	for x in range(num-2):
+		if(num%(x+2)==0):
+			return False
+	return True
+
 def rotate(num):
 	x=0
 	strg=str(num)
@@ -8,6 +14,11 @@ def rotate(num):
 	while(newStr!=strg):
 		x+=1
 		newStr=strg[x:] + strg[:x]
-		print (int(strg[x:] + strg[:x]))
+		if isPrime(int(strg[x:] + strg[:x]))==False:
+			return False
+	return True
 	
-rotate(197)
+total=0
+for x in range(99):
+	if(rotate(x+2)): total+=1
+print(total)
