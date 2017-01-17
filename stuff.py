1d0a8c15036db3b73128a66250ef43a63f342097
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-01-16 20:47:35 -0600

    Update stuff.py

--- stuff.py
+++ stuff.py
@@ -1,16 +1,16 @@
 def num(starting):
 	length=len(str(starting))
 	place=[" "," "," hundred "," thousand "," "," hundred "," million "," "," hundred "," billion "]
-	ones=["","one","two","three","four","five","six","seven","eight","nine"]
+	ones=["zero","one","two","three","four","five","six","seven","eight","nine"]
 	tens=["","onety","twenty","thirty","fourty","fifty","sixty","seventy","eighty","ninty"]
 	wrd=""
 	i=length-1
 	while(i>=0):
 		i-=1
-		if(length-i==1 or length-i==4 or length-i==7):
-			wrd=tens[int(str(starting)[i])]+place[length-i-2]+wrd
+		if(length-i-2==0 or length-i==4 or length-i==7):
+			wrd=wrd+tens[int(str(starting)[i])]+place[length-i-2]
 		else:
-			wrd=ones[int(str(starting)[i])]+place[length-i-2]+wrd
+			wrd=wrd+ones[int(str(starting)[i])]+place[length-i-2]
 	print(wrd)
 
 num(1)
