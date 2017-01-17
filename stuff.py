440e81db57bd0c8528de80633cfb6d8f40df8309
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-01-16 20:27:07 -0600

    Update stuff.py

--- stuff.py
+++ stuff.py
@@ -1,16 +1,16 @@
 def num(starting):
 	length=len(str(starting))
-	place={" "," "," hundred "," thousand "," "," hundred "," million "," "," hundred "," billion "}
-	ones={"","one","two","three","four","five","six","seven","eight","nine"}
-	tens={"","onety","twenty","thirty","fourty","fifty","sixty","seventy","eighty","ninty"}
+	place=[" "," "," hundred "," thousand "," "," hundred "," million "," "," hundred "," billion "]
+	ones=["","one","two","three","four","five","six","seven","eight","nine"]
+	tens=["","onety","twenty","thirty","fourty","fifty","sixty","seventy","eighty","ninty"]
 	wrd=""
 	i=length-1
 	while(i>=0):
 		i-=1
-		if(length-i-1==1 or length-i-1==4 or length-i-1==7):
-			wrd=tens[str(starting)[i]]+place[length-i-1]+wrd
+		if(length-i==1 or length-i==4 or length-i==7):
+			wrd=tens[int(str(starting)[i])]+place[length-i-1]+wrd
 		else:
-			wrd=ones[str(starting)[i]]+place[length-i-1]+wrd
+			wrd=ones[int(str(starting)[i])]+place[length-i-1]+wrd
 	print(wrd)
 
 num(1)
