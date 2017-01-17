fff633ae5ffb59b6811708ba7f756d7fad4f2eb3
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-01-16 20:07:53 -0600

    Create stuff.py

---
+++ stuff.py
@@ -0,0 +1,22 @@
+def num(starting):
+	length=len(str(starting))
+	place={" "," "," hundred "," thousand "," "," hundred "," million "," "," hundred "," billion "}
+	ones={"","one","two","three","four","five","six","seven","eight","nine"}
+	tens={"","onety","twenty","thirty","fourty","fifty","sixty","seventy","eighty","ninty"}
+	wrd=""
+	i=length-1
+	while(i>=0):
+		i-=1
+		if(length-i-1==1 or length-i-1==4 or length-i-1==7):
+			wrd=tens[str(starting)[i]]+place[length-i-1]+wrd
+		else:
+			wrd=ones[str(starting)[i]]+place[length-i-1]+wrd
+	print(wrd)
+
+num(1)
+num(2)
+num(10)
+num(12)
+num(50)
+num(100)
+num(666)
