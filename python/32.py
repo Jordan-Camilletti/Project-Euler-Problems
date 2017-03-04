95586ae8cb21cdad76c7fe1777a94fcfc1d4dbfb
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-03-03 21:33:51 -0600

    Update 32.py

--- python/32.py
+++ python/32.py
@@ -1,17 +1,19 @@
-def abundant(n):
+def a(n):
     t=0
     for x in range(n-1):
         if(n%(x+1)==0):
             t+=x+1
             if(t>n): return True
     return False
-	 
-total=0
-for m in range(20162):
-	total+=(m+1)
-	print(m)
-	for n in range(m+1):
-		if(perf((m+1)-(n+1))==True and perf(n+1)==True):
-			total-=(m+1)
-			break
-print(total)
+
+def g(g):
+    for n in range(g):
+        if(a(n+1) and a(g-n-1)): return False
+    return True
+    
+    
+#print(g(24)==False and g(25)==True and g(36)==False and g(35)==True and g(40)==False and g(41)==True)
+t=0
+for n in range(20161):
+    if(!g(n+1)): t+=n+1
+print(t)
