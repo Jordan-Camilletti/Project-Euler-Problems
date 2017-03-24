84309c24221220ed997d0fe7bee3bb2741a19355
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-03-24 11:21:10 -0500

    Update 30. Digit fifth powers.py

--- python/30. Digit fifth powers.py
+++ python/30. Digit fifth powers.py
@@ -1,10 +1,20 @@
-"""
-"""
+"""Surprisingly there are only three numbers that can be written as the sum of fourth powers of their digits:
+
+1634 = 14 + 64 + 34 + 44
+8208 = 84 + 24 + 04 + 84
+9474 = 94 + 44 + 74 + 44
+As 1 = 14 is not a sum it is not included.
+
+The sum of these numbers is 1634 + 8208 + 9474 = 19316.
+
+Find the sum of all the numbers that can be written as the sum of fifth powers of their digits."""
 
 num=0
 tot=0
-for x in range(1,100):
+for x in range(1,443841):
 	score=0
-	for n in str(x): score+=(int(n)**5)
+	for n in str(x): 
+		score+=(int(n)**5)
+		if(n=="1"): score-=1
 	if(score==x): tot+=x
 print(tot)
