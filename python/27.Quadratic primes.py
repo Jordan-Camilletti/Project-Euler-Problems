cbcfa26c7437c785d08a669f6a46cf0704a0d968
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-05-15 11:12:17 -0500

    Update 27.Quadratic primes.py

--- python/27.Quadratic primes.py
+++ python/27.Quadratic primes.py
@@ -7,9 +7,17 @@ def prime(num):
 			return False
 	return True
 
-a=-79
-b=1601
-n=0
-while(prime((n*n)+(a*n)+b)):
-	n+=1
-print(n)
+top=0
+topA=0
+topB=0
+for a in range(-999,1000):
+	for b in range(-1000,1000):
+		n=0
+		while(prime((n*n)+(a*n)+b)):
+			n+=1
+		if(n>top):
+			top=n
+			topA=a
+			topB=b
+
+print(topA*topB)
