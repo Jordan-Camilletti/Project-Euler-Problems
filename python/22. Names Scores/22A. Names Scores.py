067ad490a8c6f8e0636cbfdddf163d9756e9afe3
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-07-17 15:07:53 -0500

    Update 22A. Names Scores.py

--- python/22. Names Scores/22A. Names Scores.py
+++ python/22. Names Scores/22A. Names Scores.py
@@ -9,60 +9,9 @@ So, COLIN would obtain a score of 938 × 53 = 49714.
 What is the total of all the name scores in the file?"""
 
 def alph(letter):
-	if(letter=="A"):
-		return 1;
-	elif(letter=="B"):
-		return 2;
-	elif(letter=="C"):
-		return 3;
-	elif(letter=="D"):
-		return 4;
-	elif(letter=="E"):
-		return 5
-	elif(letter=="F"):
-		return 6
-	elif(letter=="G"):
-		return 7
-	elif(letter=="H"):
-		return 8
-	elif(letter=="I"):
-		return 9
-	elif(letter=="J"):
-		return 10
-	elif(letter=="K"):
-		return 11
-	elif(letter=="L"):
-		return 12
-	elif(letter=="M"):
-		return 13
-	elif(letter=="N"):
-		return 14
-	elif(letter=="O"):
-		return 15
-	elif(letter=="P"):
-		return 16
-	elif(letter=="Q"):
-		return 17
-	elif(letter=="R"):
-		return 18
-	elif(letter=="S"):
-		return 19
-	elif(letter=="T"):
-		return 20
-	elif(letter=="U"):
-		return 21
-	elif(letter=="V"):
-		return 22
-	elif(letter=="W"):
-		return 23
-	elif(letter=="X"):
-		return 24
-	elif(letter=="Y"):
-		return 25
-	elif(letter=="Z"):
-		return 26
-	else:
-		return 0
+	alphabet=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+	for n in range(len(alphabet)):
+		if(alphabet[n]==letter): return n+1
 
 def nameScore(name):
 	tot=0
