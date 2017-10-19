80cf7138c4cf2eea16909141a3f1f523a3f15e52
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-10-19 10:17:23 -0500

    Update 48. Self powers.py

--- python/48. Self powers.py
+++ python/48. Self powers.py
@@ -1,8 +1,8 @@
-/*The series, 11 + 22 + 33 + ... + 1010 = 10405071317.
+"""The series, 11 + 22 + 33 + ... + 1010 = 10405071317.
 
-Find the last ten digits of the series, 11 + 22 + 33 + ... + 10001000.*/
+Find the last ten digits of the series, 11 + 22 + 33 + ... + 10001000."""
 
-tot=0
-for n in range(1,1001):
-	tot+=(n**n)
-print(str(tot)[-10:])
+tot=0#Yep, this is it
+for n in range(1,1001):#This is the entire solution
+	tot+=(n**n)#Just four lines
+print(str(tot)[-10:])#This is all there is to the problem
