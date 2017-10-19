78685ca9c5520f8617acb3cd5ca2e91b88a76a0d
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-10-19 11:11:36 -0500

    Update and rename 49. 94 to 49. Prime permutations.py

--- python/49. Prime permutations.py
+++ python/49. Prime permutations.py
@@ -1,5 +1,8 @@
-"""
-"""
+"""The arithmetic sequence, 1487, 4817, 8147, in which each of the terms increases by 3330, is unusual in two ways: (i) each of the three terms are prime, and, (ii) each of the 4-digit numbers are permutations of one another.
+
+There are no arithmetic sequences made up of three 1-, 2-, or 3-digit primes, exhibiting this property, but there is one other 4-digit increasing sequence.
+
+What 12-digit number do you form by concatenating the three terms in this sequence?"""
 
 from itertools import permutations
 
@@ -15,8 +18,3 @@ for number in range(1001,9998,2):
 		if(isPrime(number) and isPrime(number+increase) and isPrime(number+increase+increase)):
 			if((tuple(str(number+increase)) in perms) and (tuple(str(number+increase+increase)) in perms)):
 				print(str(number)+str(number+increase)+str(number+increase+increase))
-
-"""perms=list(permutations(str(number)))
-print(tuple(str(number)) in perms)
-print(tuple(str(number+increase)) in perms)
-print(tuple(str(number+increase+increase)) in perms)"""
