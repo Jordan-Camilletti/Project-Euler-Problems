68e6e593eebe53ad67b70fb289201963784d6fc6
Jordan <jordan.camilletti@gmail.com>
2018-10-07 13:51:27 -0500

    .

--- python/63. 36.py
+++ python/63. 36.py
@@ -2,18 +2,3 @@
 
 How many n-digit positive integers exist which are also an nth power?"""
 
-def isPow(num):
-	pwr=1
-	while(pow(pwr,len(str(num)))<num):
-		pwr+=1
-	if(pow(pwr,len(str(num)))!=num):
-		return(0)
-	return(pwr)
-
-sum=0
-digit=0
-while(isPow(digit)!=1):
-	if(isPow(digit)>1):
-		sum+=1
-	digit+=1
-print(sum)
