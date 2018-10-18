759ceb745ed1cac19dd89f653599eb24e392610c
Jordan <jordan.camilletti@gmail.com>
2018-10-18 00:06:57 -0500

    currNum removed as number at the end of the array works just as well

--- python/58. Spiral primes.py
+++ python/58. Spiral primes.py
@@ -3,9 +3,7 @@
 
 length=7
 cornerNums=[1]
-currNum=1
 for n in range(int((length-1)/2)):
 	for corner in range(4):
-		currNum+=((n+1)*2)
-		cornerNums.append(currNum)
+		cornerNums.append(cornerNums[-1]+((n+1)*2))
 print(cornerNums)
