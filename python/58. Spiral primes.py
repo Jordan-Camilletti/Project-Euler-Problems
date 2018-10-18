5e59505482afdd8090bf9e99e61aa66c70d2be4c
Jordan <jordan.camilletti@gmail.com>
2018-10-18 00:13:32 -0500

    optimized prime function as no tested numbers will be even

--- python/58. Spiral primes.py
+++ python/58. Spiral primes.py
@@ -1,6 +1,15 @@
 """
 """
 
+def isPrime(num):
+	for n in range(3,int(num/2),2):
+		if(num%n==0):
+			return(False)
+	return(True)
+
+for test in range(20):
+	print(test," ",isPrime(test))
+
 length=7
 cornerNums=[1]
 for n in range(int((length-1)/2)):
