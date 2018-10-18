e97d58da739b403b4c61dfe24a461e4b0e001951
Jordan <jordan.camilletti@gmail.com>
2018-10-17 23:56:23 -0500

    Array now appends instead of asigns

--- python/58. Spiral primes.py
+++ python/58. Spiral primes.py
@@ -2,9 +2,10 @@
 """
 
 length=3
-spiral=[[(length*length)-(2*(length-1))]]
+spiral=[[]]
 for n1 in range(length):
+	spiral.append([])
 	for n2 in range(length):
 		currNum=(length*length)-(2*(length-1))+n1
-		spiral[n1][n2]=
+		spiral[n1].append(currNum-n2)
 print(spiral)
