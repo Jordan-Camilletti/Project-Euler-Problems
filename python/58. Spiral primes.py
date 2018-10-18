eb4fc23ba48013af8235524cdbd867d0590961e6
Jordan <jordan.camilletti@gmail.com>
2018-10-17 23:54:17 -0500

    .

--- python/58. Spiral primes.py
+++ python/58. Spiral primes.py
@@ -2,4 +2,9 @@
 """
 
 length=3
-spiral=[[]]
+spiral=[[(length*length)-(2*(length-1))]]
+for n1 in range(length):
+	for n2 in range(length):
+		currNum=(length*length)-(2*(length-1))+n1
+		spiral[n1][n2]=
+print(spiral)
