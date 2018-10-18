81f67463a41c9c0160f5044e4eb8294fbe7bcfa3
Jordan <jordan.camilletti@gmail.com>
2018-10-18 00:04:27 -0500

    Increased corner incentive

--- python/58. Spiral primes.py
+++ python/58. Spiral primes.py
@@ -4,8 +4,8 @@
 length=7
 cornerNums=[1]
 currNum=1
-for n in range((length-1)/2):
+for n in range(int((length-1)/2)):
 	for corner in range(4):
-		currNum+=(n*2)
+		currNum+=((n+1)*2)
 		cornerNums.append(currNum)
 print(cornerNums)
