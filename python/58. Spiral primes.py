59012c95bd142163e07332ad6ddb4bce8f7323d4
Jordan <jordan.camilletti@gmail.com>
2018-10-18 00:16:00 -0500

    Removed test cases

--- python/58. Spiral primes.py
+++ python/58. Spiral primes.py
@@ -2,17 +2,19 @@
 """
 
 def isPrime(num):
+	if(num==1):
+		return(False)
 	for n in range(3,int(num/2),2):
 		if(num%n==0):
 			return(False)
 	return(True)
-
-for test in range(20):
-	print(test," ",isPrime(test))
-
+	
 length=7
 cornerNums=[1]
 for n in range(int((length-1)/2)):
 	for corner in range(4):
 		cornerNums.append(cornerNums[-1]+((n+1)*2))
-print(cornerNums)
+
+for num in cornerNums:
+	if(isPrime(num)):
+		print(num)
