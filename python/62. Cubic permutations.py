e8348ed7cff88b1b519751df25521452a2d07495
Jordan Camilletti <jordan.camilletti@gmail.com>
2019-01-02 20:55:15 -0600

    Added some comments

--- python/62. Cubic permutations.py
+++ python/62. Cubic permutations.py
@@ -5,17 +5,17 @@ Find the smallest cube for which exactly five permutations of its digits are cub
 def translateNum(num):
 	return(''.join(sorted(str(num))))
 
-cubes=[]
-used=[]
+cubes=[]#I know I could have combined 'cubes' and 'used' into a 2D array
+used=[]#but 2 arrays is easier to work with when searching in each of them
 num=0
 n=0
-while(5 not in used):#5 not in [i[1] for i in cubes]
+while(5 not in used):#Number of permutation cubes wanted
 	if(translateNum(num**3) in cubes):
 		used[cubes.index(translateNum(num**3))]+=1
 	else:
 		cubes.append(translateNum(num**3))
 		used.append(1)
-	num+=1
-while(translateNum(n**3)!=translateNum((num-1)**3)):
+	num+=1#Finding the largest of the permutation cubes w/ sorted numbers
+while(translateNum(n**3)!=translateNum((num-1)**3)):#Once the largest of the cubes is found, this loop will find the smallest
 	n+=1
 print(n**3)
