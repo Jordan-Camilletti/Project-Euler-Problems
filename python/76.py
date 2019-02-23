d34421150abcbcd568e3cd26aa53f9efa72aa411
Jordan <jordan.camilletti@gmail.com>
2019-02-22 23:04:48 -0600

    Changed function input

--- python/76.py
+++ python/76.py
@@ -1,9 +1,9 @@
 """
 """
 
-def ways(num1,num2):
+def ways(num1,num2=0):
 	if(num1<=1 and num2<=1)
 		return(1)
 	if(num1<=num2)
-		return(1+ways(num2,0))
+		return(1+ways(num2))
 	return(1+ways(num1-1,num2+1))
