b70aee7d9f73dd86475824e6dca9077b95e31d41
Jordan <jordan.camilletti@gmail.com>
2019-02-23 22:37:10 -0600

    Changed major for loop and removed some tests

--- python/76.py
+++ python/76.py
@@ -1,18 +1,14 @@
 """
 """
 
-def ways(num1,num2=0):
+def ways(num1,num2=0,t=0):
 	print(str(num1)+" X "+str(num2))
-	if(num1==1):
-		if(num2<1):
-			return(0)
-		return(1)
-	if(num1<1 or num1<num2):
+	if(num1<num2):
 		return(0)
-	if(num2>1)
-		return(1+ways(num2-1,1))
+	if(num1==num2+1):
+		return(1+ways(num2+1))
 	return(1+ways(num1-1,num2+1))
 	
 
 print(ways(5))
-print(ways(100))
+#print(ways(100))
