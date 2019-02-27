f59e073ac896030b791024b063dd8ed2eb1f0ee5
Jordan Camilletti <jordan.camilletti@gmail.com>
2019-02-26 23:56:23 -0600

    Update 76.py

--- python/76.py
+++ python/76.py
@@ -3,12 +3,8 @@
 
 def ways(num1,num2=0):
 	print(str(num1)+" X "+str(num2))
-	if(num1<num2):
+	if(num1<=1 or num2>num1):
 		return(0)
-	if(num2<=0):
-		return(ways(num1-1,num2+1))
-	if(num1==num2+1):
-		return(1+ways(num2)+ways(num1,num2-1))
 	return(1+ways(num1-1,num2+1))
 
 print(ways(5))
