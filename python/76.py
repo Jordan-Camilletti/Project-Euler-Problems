0b4deb529efc2d6371a4a54c2f59400f1c9df805
Jordan <jordan.camilletti@gmail.com>
2019-02-27 23:56:09 -0600

    Changed test cases

--- python/76.py
+++ python/76.py
@@ -6,9 +6,11 @@ def ways(num1,num2=0):
 	if(num2>num1 or num1<=1):
 		return(0)
 	if(num2<=0):
-		return(1+ways(num1-1,1))
-	if(num1==num2+1):
-		return(1+ways(num1-1,num2+1))
-	return(1+ways(num1,num2-1)+ways(num2-1,1))
-
+		return(ways(num1-1,1))
+	if(num1<=num2+1):
+		print("P")
+		return(1+ways(num1,num2-1)+ways(num2-1,1))
+	print("P")
+	return(1+ways(num1-1,num2+1))
+	
 print(ways(5))
