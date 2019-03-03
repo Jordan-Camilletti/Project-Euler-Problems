a132cbfb8275f1f1088e43213f26045df4bbd462
Jordan <jordan.camilletti@gmail.com>
2019-03-03 16:49:16 -0600

    Added more print statements

--- python/76.py
+++ python/76.py
@@ -1,18 +1,19 @@
 """https://projecteuler.net/problem=76
 """
 
-num=5
+n=5
 ways=0
-nums=[0]*num
-nums[0]=num
+nums=[0]*n
+nums[0]=n
 while(0 in nums):
 	print(nums)
 	ways+=1
-	for n in range(len(nums)-1,-1,-1):
-		if(num[n]==1 and n!=len(nums)-1):
-			nums[n+1]=1
-		elif(num[n]!=0):
-			nums[n+1]+=1
-			nums[n]-=1
+	for num in range(len(nums)-1,-1,-1):
+		#print(str(num)+" X "+str(nums[num]))
+		if(nums[num]==1 and n!=len(nums)-1):
+			nums[num+1]=1
+		elif(nums[num]!=0):
+			nums[num+1]+=1
+			nums[num]-=1
 			break
-			
+print(ways)
