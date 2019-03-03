e4be3c3708be87229f03797758733fe9804489f6
Jordan <jordan.camilletti@gmail.com>
2019-03-03 00:02:21 -0600

    Changed algorithm from recursion to loop

--- python/76.py
+++ python/76.py
@@ -6,14 +6,14 @@ ways=0
 nums=[0]*num
 nums[0]=num
 print(nums)
-#while(0 in nums):
-print(nums)
-ways+=1
-for n in range(len(nums)-1,-1,-1):
-	if(num[n]==1 and n!=len(nums)-1):
-		nums[n+1]=1
-	elif(num[n]!=0):
-		nums[n+1]+=1
-		nums[n]-=1
-		break
-		
+while(0 in nums):
+	print(nums)
+	ways+=1
+	for n in range(len(nums)-1,-1,-1):
+		if(num[n]==1 and n!=len(nums)-1):
+			nums[n+1]=1
+		elif(num[n]!=0):
+			nums[n+1]+=1
+			nums[n]-=1
+			break
+			
