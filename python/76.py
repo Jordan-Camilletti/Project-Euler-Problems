6f8b14c3d7c4e0dd0c073fdf206613e5e8d333e4
Jordan <jordan.camilletti@gmail.com>
2019-03-02 23:50:03 -0600

    Added more test cases

--- python/76.py
+++ python/76.py
@@ -2,14 +2,18 @@
 """
 
 def ways(num1,num2=0):
+	print(str(num1)+" X "+str(num2))
 	if(num1<num2):
 		return(0)
 	if(num1==1 and num2==0):
+		print("\n")
 		return(1)
 	if(num2==0):
-		return(1+ways(num2-1,1))
-	print(str(num1)+" X "+str(num2))
-	return(
+		print("\n")
+		return(1+ways(num1-1,1))
+	if(num1>num2+1):
+		return(ways(num1-1,1)+ways(num2-1))
+	return(ways(num1-1,num2+1)+ways(num2))
 	#return(ways(num2)+ways(num1-1,num2+1))
 	
 print(ways(5))
