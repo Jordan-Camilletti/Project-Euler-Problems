46cd58ff0ba14f25f6f2869fdf926a416c1d38b1
Jordan <jordan.camilletti@gmail.com>
2019-03-03 18:25:16 -0600

    Added twoToOne method

--- python/76.py
+++ python/76.py
@@ -1,14 +1,24 @@
 """https://projecteuler.net/problem=76
 """
 
+def twoToOne(nums):#Transforming 2,1 to 1,1,1
+	rtnNum=nums
+	for num in range(len(rtnNums)-1):
+		if(rtnNum[num]==2 and rtnNum[num+1]==1):
+			for n in range(len(nums)-1,num+2,-1):
+				rtnNum[n]=rtnNum[n-1]
+			for n in range(num,num+3):
+				rtnNum[num]=1
+
 n=5
 ways=0
 nums=[0]*n
 nums[0]=n
+move=False
 while(0 in nums):
-	print(nums)
 	ways+=1
-	for num in range(len(nums)-1,-1,-1):
+	move=False
+	for num in range(len(nums)):
 		#print(str(num)+" X "+str(nums[num]))
 		if(nums[num]==1 and n!=len(nums)-1):
 			nums[num+1]=1
@@ -16,4 +26,5 @@ while(0 in nums):
 			nums[num+1]+=1
 			nums[num]-=1
 			break
+	print(nums)
 print(ways)
