9ba21ed30d6f5f757d06266fc84bdcce01a3aada
Jordan <jordan.camilletti@gmail.com>
2019-03-09 01:32:51 -0600

    Added test print statements

--- python/76.py
+++ python/76.py
@@ -8,10 +8,13 @@ def mainFindWays(mainNum,num):
 def subFindWays(mainNum,numA):
 	numB=mainNum-numA
 	if(numB<=2):#If numB is 1 or 2, return that number
+		print(("X"*numB)+str(numA)+" : "+str(numB))
 		return(numB)
 	if(numB>numA):
+		print(str(numA)+" : "+str(numB))
 		return(mainFindWays(numA,numA-1)+mainFindWays(numB,numB-1))
 	else:
+		print("X"+str(numA)+" : "+str(numB))
 		return(1+mainFindWays(numA,numA-1)+mainFindWays(numB,numB-1))
 	
 print(mainFindWays(5,4))
