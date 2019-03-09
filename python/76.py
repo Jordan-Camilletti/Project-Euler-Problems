39d3a58a45a020c8d19e83bb125eeb8d0ec427a5
Jordan <jordan.camilletti@gmail.com>
2019-03-09 01:13:01 -0600

    Added comments to the subFindWays method

--- python/76.py
+++ python/76.py
@@ -1,11 +1,15 @@
 """https://projecteuler.net/problem=76"""
 
-def mainFindWays(mainNum,num=mainNum-1):
-	if(num<1 or num>=mainNum):
-		return(0)
-	return(subFindWays(num)+mainFindWays(mainNum,num-1))
+def mainFindWays(mainNum,num):
+	if(num<=1):
+		return(1)
+	return(subFindWays(mainNum,num)+mainFindWays(mainNum,num-1))
 	
-def subFindWays():
+def subFindWays(mainNum,numA):
+	numB=mainNum-numA
+	if(numB<=2):#If numB is 1 or 2, return that number
+		return(numB)
+	print(str(numA)+" : "+str(numB))
 	return(0)
 	
-print(mainFindWays(5))
+print(mainFindWays(5,4))
