ff14ba671a52c0188759e4d843369233f761f396
Jordan <jordan.camilletti@gmail.com>
2019-03-09 21:26:51 -0600

    Welp, my last code was trash

--- python/76.py
+++ python/76.py
@@ -1,15 +1,6 @@
 """https://projecteuler.net/problem=76"""
 
-num=6
-currNum=num
-ways=1
-inc=0
-while(currNum>=num/2):
-	ways+=inc
-	inc+=1
-	currNum-=1
-inc-=1
-while(currNum>1):
-	ways+=inc
-	currNum-=1
-print(ways)
+num=10
+nums=[0]*num
+nums[0]=num
+print(nums)
