21daa40a2e0c761e21cce1d04103680366508965
Jordan <jordan.camilletti@gmail.com>
2019-03-11 23:30:07 -0500

    Added links and basic vars

--- python/79.py
+++ python/79.py
@@ -1,1 +1,7 @@
-"""https://projecteuler.net/problem=79"""
+"""https://projecteuler.net/problem=79
+https://projecteuler.net/project/resources/p079_keylog.txt"""
+
+def addLog(code,log):
+	return()
+
+code=[]
