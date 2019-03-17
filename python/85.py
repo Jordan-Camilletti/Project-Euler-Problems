4420246b2d7ef381c7c035114976076c6778756a
Jordan <jordan.camilletti@gmail.com>
2019-03-16 22:51:41 -0500

    Added equation for finding how many rectangles are in it

--- python/85.py
+++ python/85.py
@@ -1,10 +1,14 @@
-def findWaysB(xLen,yLen)
+def findWaysB(xLen,yLen,maxX,maxY):
+	return(
 
 def findWaysA(maxXLen,maxYLen):
 	ways=0
 	for xLen in range(1,maxXLen+1):
 		for yLen in range(1,maxYLen+1):
+			ways+=(maxXLen-xLen+1)*(maxYLen-yLen+1)
+			#ways+=findWaysB(xLen,yLen,maxXLen,maxYLen)
 			print("X: "+str(xLen)+"\nY: "+str(yLen)+"\n")
+	return(ways)
 
 tot=0
 findWaysA(5,2)
