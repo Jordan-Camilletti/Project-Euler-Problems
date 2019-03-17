92d849ae89d74359e4125753f04c2bdb10b29454
Jordan <jordan.camilletti@gmail.com>
2019-03-16 22:21:47 -0500

    Added two findWays methods

--- python/85.py
+++ python/85.py
@@ -1,8 +1,10 @@
-def findWays(maxXLen,maxYLen):
+def findWaysB(xLen,yLen)
+
+def findWaysA(maxXLen,maxYLen):
 	ways=0
 	for xLen in range(1,maxXLen+1):
 		for yLen in range(1,maxYLen+1):
-			print("X: "+str(xLen)+"\nY: "+str(yLen))
+			print("X: "+str(xLen)+"\nY: "+str(yLen)+"\n")
 
 tot=0
-findWays(5,0)
+findWaysA(5,2)
