658021563af820acde417933a39a57d3e73ea418
Jordan <jordan.camilletti@gmail.com>
2019-03-16 22:53:14 -0500

    Added TODOs

--- python/85.py
+++ python/85.py
@@ -1,14 +1,10 @@
-def findWaysB(xLen,yLen,maxX,maxY):
-	return(
-
 def findWaysA(maxXLen,maxYLen):
 	ways=0
 	for xLen in range(1,maxXLen+1):
 		for yLen in range(1,maxYLen+1):
-			ways+=(maxXLen-xLen+1)*(maxYLen-yLen+1)
-			#ways+=findWaysB(xLen,yLen,maxXLen,maxYLen)
-			print("X: "+str(xLen)+"\nY: "+str(yLen)+"\n")
+			ways+=(maxXLen-xLen+1)*(maxYLen-yLen+1)#TODO: Test these
+			#print("X: "+str(xLen)+"\nY: "+str(yLen)+"\n")
 	return(ways)
 
 tot=0
-findWaysA(5,2)
+print(findWaysA(5,2))
