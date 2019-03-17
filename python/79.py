16e45ea8ef8079c0d9009752ca2c1c0dbdb447b6
Jordan <jordan.camilletti@gmail.com>
2019-03-16 21:44:46 -0500

    Added the basics of 85.py

--- python/79.py
+++ python/79.py
@@ -2,12 +2,15 @@
 https://projecteuler.net/project/resources/p079_keylog.txt"""
 
 def addLog(code,log):
+	n1=log[0]
+	n2=log[1]
+	n3=log[2]
+	print("X"+log+"X")
 	code+=log
 	return()
 
 code=[]
 with open("keylog.txt","r+") as lines:
 	for line in lines:
-		print(line)
-		#code=addLog(code,line)
+		addLog(code,line[:-1])
 print(len(code))
--- python/85.py
+++ python/85.py
@@ -1,2 +1,8 @@
-"""
-"""
+def findWays(maxXLen,maxYLen):
+	ways=0
+	for xLen in range(1,maxXLen+1):
+		for yLen in range(1,maxYLen+1):
+			print("X: "+str(xLen)+"\nY: "+str(yLen))
+
+tot=0
+findWays(5,0)
