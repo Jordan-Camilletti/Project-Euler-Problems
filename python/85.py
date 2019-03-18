74bea3c288f7ad585e3256a65d530c15188ca190
Jordan Camilletti <jordan.camilletti@gmail.com>
2019-03-17 21:37:04 -0500

    Update 85.py

--- python/85.py
+++ python/85.py
@@ -2,7 +2,7 @@ def findWays(maxXLen,maxYLen):
 	ways=0
 	for xLen in range(1,maxXLen+1):
 		for yLen in range(1,maxYLen+1):
-			ways+=(maxXLen-xLen+1)*(maxYLen-yLen+1)#TODO: Test this
+			ways+=(maxXLen-xLen+1)*(maxYLen-yLen+1)
 			#print("X: "+str(xLen)+"\nY: "+str(yLen)+"\n")
 	return(ways)
 
@@ -10,3 +10,8 @@ areaX=1
 areaY=1
 print(findWays(3,2))
 #TODO: add main loops for areaX and areaY
+while(findWays(areaX,areaY)):
+	areaY=1
+	while(areaY<areaX):
+		areaY++
+	areaX++
