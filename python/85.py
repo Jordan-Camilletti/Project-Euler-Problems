4ca4044b92b49c4a44785936750fd9d847dd4dee
Jordan <jordan.camilletti@gmail.com>
2019-03-27 23:42:22 -0500

    Removed some useless comments

--- python/85.py
+++ python/85.py
@@ -1,23 +1,24 @@
+"""
+https://projecteuler.net/problem=85
+"""
+
 def findWays(maxXLen,maxYLen):
 	ways=0
 	for xLen in range(1,maxXLen+1):
 		for yLen in range(1,maxYLen+1):
-			ways+=(maxXLen-xLen+1)*(maxYLen-yLen+1)
 			#print("X: "+str(xLen)+"\nY: "+str(yLen)+"\n")
+			ways+=(maxXLen-xLen+1)*(maxYLen-yLen+1)
 	return(ways)
 
 areaX=1
 areaY=1
-while(findWays(areaX,areaY)<2000000):
-	#print(str(areaX)+" "+str(findWays(areaX,areaY)))
-	"""areaX+=1
-	areaY=areaX"""
-	areaY=1
-	while(areaY<areaX):#TODO: combind this loop into the main loop
-		if(findWays(areaX,areaY)>2000000):	
-			print(str(areaX*areaY)+" "+str(findWays(areaX,areaY)))
-		areaY+=1
-	areaX+=1
-print("\nX: "+str(areaX)+"\nY: "+str(areaY)+"\nArea: "+str(areaX*areaY)+"\n"+str(findWays(areaX,areaY))) 
+while(findWays(areaX,areaY)<=2000000):
+	print(str(areaX)+" "+str(findWays(areaX,areaY)))
+	areaY+=1
+	if(areaY>areaX):
+		areaX+=1
+		areaY=1
+		
+print("\nX: "+str(areaX)+"\nY: "+str(areaY)+"\nArea: "+str(areaX*areaY)+"\n"+str(2000000-findWays(areaX,areaY))) 
 areaY-=1
-print("\nX: "+str(areaX)+"\nY: "+str(areaY)+"\nArea: "+str(areaX*areaY)+"\n"+str(findWays(areaX,areaY))) 
+print("\nX: "+str(areaX)+"\nY: "+str(areaY)+"\nArea: "+str(areaX*areaY)+"\n"+str(2000000-findWays(areaX,areaY))) 
