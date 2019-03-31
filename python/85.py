c0e9dd6dc3d19133693aad2b556558d8683489a9
Jordan Camilletti <jordan.camilletti@gmail.com>
2019-03-30 23:58:01 -0500

    Update 85.py

--- python/85.py
+++ python/85.py
@@ -1,5 +1,9 @@
 """
 https://projecteuler.net/problem=85
+By counting carefully it can be seen that a rectangular grid measuring 3 by 2 contains eighteen rectangles:
+
+
+Although there exists no rectangular grid that contains exactly two million rectangles, find the area of the grid with the nearest solution.
 """
 
 def findWays(maxXLen,maxYLen):
