1fa4602ecb3934b3b3f0e465031b186f5e1af695
Jordan <jordan.camilletti@gmail.com>
2019-07-01 23:39:37 -0500

    Improved syntax of methods

--- python/92.py
+++ python/92.py
@@ -10,8 +10,9 @@ Therefore any chain that arrives at 1 or 89 will become stuck in an endless loop
 
 How many starting numbers below ten million will arrive at 89?
 """
-# Returns index of x in arr if present, else -1 
-def binarySearch (arr, l, r, x): 
+
+# Returns index of num in arr if present, else -1 
+"""def binarySearch (arr, num, l, r): 
   
     # Check base case 
     if r >= l: 
@@ -19,24 +20,45 @@ def binarySearch (arr, l, r, x):
         mid = l + (r - l)/2
   
         # If element is present at the middle itself 
-        if arr[mid] == x: 
+        if arr[mid] == num: 
             return mid 
           
         # If element is smaller than mid, then it can only 
         # be present in left subarray 
-        elif arr[mid] > x: 
-            return binarySearch(arr, l, mid-1, x) 
+        elif arr[mid] > num: 
+            return binarySearch(arr, l, mid-1, num) 
   
         # Else the element can only be present in right subarray 
         else: 
-            return binarySearch(arr, mid+1, r, x) 
+            return binarySearch(arr, mid+1, r, num) 
   
     else: 
         # Element is not present in the array 
-        return -1
+        return -1"""
 		
-def binarySearch(arr,n,l,r):
-	mid=(l+(r-1))/2
+def binarySearch (arr, num, l, r): 
+  
+    # Check base case 
+    if r >= l: 
+  
+        mid = l + (r - l)/2
+  
+        # If element is present at the middle itself 
+        if arr[mid] == num: 
+            return(True)
+          
+        # If element is smaller than mid, then it can only 
+        # be present in left subarray 
+        elif arr[mid] > num: 
+            return(binarySearch(arr, num, l, mid-1))
+  
+        # Else the element can only be present in right subarray 
+        else: 
+            return(binarySearch(arr, num, mid+1, r))
+  
+    else: 
+        # Element is not present in the array 
+        return(False)
 		
 
 def findNewSquare(num, sol1, sol89):
@@ -46,11 +68,11 @@ def findNewSquare(num, sol1, sol89):
 	#-4=old number leads to 89
 	if(num==1):
 		return(-1)
-	if(num in sol1):#TODO: add binary search to these
+	if(binarySearch(sol1,num,0,len(sol1)-1)):#TODO: add binary search to these
 		return(-2)
 	if(num==89):
 		return(-3)
-	if(num in sol89):#TODO: add binary search to these
+	if(binarySearch(sol89,num,0,len(sol89)-1)):#TODO: add binary search to these
 		return(-4)
 	sum=0
 	while(num>=1):
