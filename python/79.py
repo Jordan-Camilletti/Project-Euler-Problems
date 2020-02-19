06704d65a408d4d334ae1afeb94ac3181b194fbb
Jordan <jordan.camilletti@gmail.com>
2020-02-18 20:40:30 -0600

    Fixed minor syntax erros

--- python/79.py
+++ python/79.py
@@ -4,11 +4,11 @@ The text file, keylog.txt, contains fifty successful login attempts.
 
 Given that the three characters are always asked for in order, analyse the file so as to determine the shortest possible secret passcode of unknown length."""
 
-def contains(int main, int value):
+def contains(main, value):
   return(false)
 
 checks=[]
 file=open("keylog.txt")
-for log in files:
+for log in file:
   checks+=log
 print(checks)
