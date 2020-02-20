5f2162fe76640419b3c53cf51bf86e00e2d19cd9
Jordan <jordan.camilletti@gmail.com>
2020-02-19 20:02:21 -0600

    combine now split into 2 functions to help with returns

--- python/79.py
+++ python/79.py
@@ -7,7 +7,7 @@ Given that the three characters are always asked for in order, analyse the file 
 """	0: not found
 	>0: value is found at end of main
 	<0: value is found at start of main
-	This returns the length of character shared between main and value"""
+	Returns the length of character shared between main and value"""
 def contains(main, value, size):
 	if(size==0 and main==value):
 		return(size)
@@ -18,16 +18,43 @@ def contains(main, value, size):
 	return(0)#Not found
 	
 """This takes a list of values amd iterates through it once
-It adds or combines these values into a 2nd array, which it then returns"""
+It adds or combines these values into a 2nd array, which it then returns
+Returns an array of combined values"""
 def combine(values):
 	combinedValues=[values[0]]
 	del(values[0])
-	for val in values:
-		for l in range(len(str(val))):
-			for cVal in combinedValues:
-				contain=contains(cVal,val,l)
-				#if(contain>0):
-				#elif(contain<0):
+	for val in range(len(values)):
+		result=addOn(val,values,combinedValues)
+		values=result[0]
+		combinedValues=result[1]
+	return(combinedValues)
+	"""for l in range(len(values[val])):
+		for cVal in range(len(combinedValues)):
+			contain=contains(combinedValues[cVal],values[val],l)
+			if(contain>0):#Add value on to the end
+				combinedValues[cVal]=combinedValues[cVal][:(-1*contain)]+values[val]
+				del(values[val])
+			elif(contain<0):#Add value on to the start
+				combinedValues[cVal]=values[val][:contain]+combinedValues[cVal]
+				del(values[val])"""
+
+"""This goes through the process of adding the 2
+It was put into its own array to allow it to stop once they combined
+Returns a 2D array, [0] is values, [1] is combinedValues"""
+def addOn(val, values, combinedValues):
+	for l in range(len(values[val])):
+		for cVal in range(len(combinedValues)):
+			contain=contains(combinedValues[cVal],values[val],l)
+			if(contain>0):#Add value on to the end
+				combinedValues[cVal]=combinedValues[cVal][:(-1*contain)]+values[val]
+				del(values[val])
+				rtn=[values,combinedValues]
+				return(rtn)
+			elif(contain<0):#Add value on to the start
+				combinedValues[cVal]=values[val][:contain]+combinedValues[cVal]
+				del(values[val])
+				rtn=[values,combinedValues]
+				return(rtn)
 
 values=[]#individual 3-len codes
 file=open("keylog.txt")
@@ -35,7 +62,7 @@ for n in file.read().split("\n"):
 	if(n+"" not in values):
 		values.append(n+"")
 file.close()
-print(values)
-"""while(len(values)>1):
+
+while(len(values)>1):
 	values=combine(values)
-print(len(values[0]))"""
+print(len(values[0]))
