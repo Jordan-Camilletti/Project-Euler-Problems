effd0801597ff3aa3b1e5af24c80c9738edeadfd
Jordan Camilletti <jordan.camilletti@gmail.com>
2020-02-23 16:04:24 -0600

    Update 79.java

--- java/79.java
+++ java/79.java
@@ -3,6 +3,7 @@
 The text file, keylog.txt, contains fifty successful login attempts.
 
 Given that the three characters are always asked for in order, analyse the file so as to determine the shortest possible secret passcode of unknown length.*/
+
 package main;
 
 import java.io.*;
