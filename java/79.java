d99827b82a48c317d5200fb824011ef4876f647e
Jordan Camilletti <jordan.camilletti@gmail.com>
2020-02-23 16:35:38 -0600

    Update 79.java

--- java/79.java
+++ java/79.java
@@ -3,15 +3,18 @@
 The text file, keylog.txt, contains fifty successful login attempts.
 
 Given that the three characters are always asked for in order, analyse the file so as to determine the shortest possible secret passcode of unknown length.*/
-
 package main;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main{
 	public static void main(String[] args){
 		ArrayList<String> values=new ArrayList<String>();
+		String password="";
+		Map<String, String> nums=new HashMap<String, String>();
 		try{
 			File f=new File("keylog");
 			BufferedReader br=new BufferedReader(new FileReader(f));
